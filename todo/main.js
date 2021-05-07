f0d4c5f6a9b2d92de10aeaf3703881c9a9f5ff80
KhaledAlabssi <khaled.absi5@gmail.com>
2021-05-07 22:42:06 +0200

    starting with js

--- todo/main.js
+++ todo/main.js
@@ -1,1 +1,45 @@
 // alert('Hi')
+const input = document.getElementById('input')
+const form = document.getElementById('form')
+const todoItem = document.getElementById('todoItem')
+const todoList = document.getElementById('todoList')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+
+
+})
+
+function addTodo (todo) {
+    let todoText = input.value
+    
+    if(todo) {
+        todoText = todo.text
+    }
+
+    if(todoText) {
+        const newTodoEl = document.createElement('li')
+
+        if(todo && todo.complited) {
+            newTodoEl.classList.add('complited')
+
+        }
+
+        newTodoEl.innerText = todoText
+
+        todoList.appendChild(newTodoEl)
+
+        input.value = ''
+
+    }
+    
+
+
+    
+
+
+}
+
+
