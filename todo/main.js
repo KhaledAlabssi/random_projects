9ac281d134db2e92b0ece1fad3e1659d4ddba588
KhaledAlabssi <khaled.absi5@gmail.com>
2021-05-08 15:49:28 +0200

    creating addTodo function

--- todo/main.js
+++ todo/main.js
@@ -1,45 +1,43 @@
-// alert('Hi')
-const input = document.getElementById('input')
-const form = document.getElementById('form')
-const todoItem = document.getElementById('todoItem')
-const todoList = document.getElementById('todoList')
+alert('Hi')
 
-form.addEventListener('submit', (e) => {
+let todos = [];
+let i1 = {data:'4:0', body: 'hi from here', complete: true}
+let i2 = {data:'5:0', body: 'hi3 from here', complete: false}
+todos.push(i1)
+todos.push(i2)
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const ul = document.getElementById('todoList')
+
+
+form.addEventListener('submit', e => {
     e.preventDefault()
 
     addTodo()
-
-
 })
 
-function addTodo (todo) {
-    let todoText = input.value
-    
-    if(todo) {
-        todoText = todo.text
-    }
-
-    if(todoText) {
-        const newTodoEl = document.createElement('li')
-
-        if(todo && todo.complited) {
-            newTodoEl.classList.add('complited')
-
-        }
-
-        newTodoEl.innerText = todoText
-
-        todoList.appendChild(newTodoEl)
-
-        input.value = ''
-
-    }
+for (let i=0; i<todos.length; i++) {
+    let li = document.createElement('li')
     
 
-
-    
-
-
+    li.innerHTML = todos[i].body
+    ul.appendChild(li)
+    console.log(li)
 }
 
+
+
+
+function addTodo (todo) {
+    // let todoText = input.value
+    // console.log(todoText)
+    let li = document.createElement('li')
+    li.classList.add('todoItem')
+    li.innerHTML = input.value
+    ul.appendChild(li)
+    input.value = ''
+
+    
+}
 
